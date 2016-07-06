026288b0c2ad67afbdc34f96d92bb8dca4fb4e6f
Jianqiu Zhang <zhangjianqiu13@gmail.com>
2016-07-06 20:09:17 +0800

    Switch from 16bit mode to 32bit protect mode

--- boot.s
+++ boot.s
@@ -1,10 +1,15 @@
 # A simple boot demo just to print a string when booting the PC
 
-.file  "boot.s"
-.code16
-.globl _start
+.file "boot.s"
 
-.text
+	CODE_SEG = gdt_code - gdt_start
+	DATA_SEG = gdt_data - gdt_start
+	VIDEO_MEM = 0xb8000
+	THEME = 0x1c
+
+	.text
+	.globl _start
+	.code16
 
 _start:
 
@@ -16,14 +21,78 @@ _start:
 loop:
 	mov (%bx), %al
 	cmp $0x00, %al
-	je  exit
+	je  to32
 	add $0x01, %bx
 	int $0x10
 	jmp loop
 
-exit:
-	jmp exit
+to32:
+	cli
+	lgdt gdt_descriptor
+	mov  %cr0, %eax
+	or   $0x1, %eax
+	mov  %eax, %cr0
+
+	ljmp $0x8, $init_pm
+
+# Construct the GDT
+
+gdt_start:
+
+gdt_null:
+	.long 0x0
+	.long 0x0
+
+gdt_code:
+	.word 0xffff
+	.word 0x0
+	.byte 0x0
+	.byte 0b10011010
+	.byte 0b11001111
+	.byte 0x0
+
+gdt_data:
+	.word 0xffff
+	.word 0x0
+	.byte 0x0
+	.byte 0b10010010
+	.byte 0b11001111
+	.byte 0x0
+
+gdt_end:
+
+# Construct the GDT Descriptor
+.zero 16
+
+gdt_descriptor:
+	.word gdt_end - gdt_start - 1
+	.long gdt_start
+	.zero 16
 
 string:
-	.string "Hello Niconico"
+	.string "Booting The Toy-OS"
 
+	.code32
+
+init_pm:
+
+	mov $DATA_SEG, %ax
+	mov %ax, %ds
+	mov %ax, %ss
+	mov %ax, %fs
+	mov %ax, %gs
+	mov %ax, %es
+
+# Try to print a character on screen
+
+mov $VIDEO_MEM, %edx
+
+print_char_pm:
+	mov $'@', %al
+	mov $THEME, %ah
+	mov %ax, (%edx)
+	add $2, %edx
+	jmp print_char_pm
+
+looop:
+	jmp looop
